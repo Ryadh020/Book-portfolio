ecf9dc7df6f9f3575d069f82d6c6d5a5d773562c
riyadh <riyadh02220@gmail.com>
2020-05-09 01:47:41 +0200

    second content circles are scrolling

--- DB.js
+++ DB.js
@@ -3,4 +3,12 @@ let competences = [
     {pic : "./Images/node.png", disc: "let js do it"},
     {pic : "./Images/wp.png", disc: "ur content is well manageed"},
     {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
+    {pic : "./Images/mySQL.png", disc: "Db talks"},
 ]
